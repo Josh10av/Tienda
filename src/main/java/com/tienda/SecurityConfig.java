47abe354051e156b52615d7aa52b3dc84fc2aafc
Josh10av <joshuaalpizarah@gmail.com>
2022-12-03 22:24:04 -0600

    Semana SecurityConfig

--- src/main/java/com/tienda/SecurityConfig.java
+++ src/main/java/com/tienda/SecurityConfig.java
@@ -1,30 +1,28 @@
 
 package com.tienda;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration 
 @EnableWebSecurity 
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Autowired
+    private UserDetailsService userDetailsService;
+    
+    
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication()
-                .withUser("juan")
-                    .password("{noop}123")
-                    .roles("ADMIN","VENDEDOR","USER")
-                .and()
-                .withUser("rebeca")
-                    .password("{noop}123")
-                    .roles("VENDEDOR","USER")
-                .and()
-                .withUser("pedro")
-                    .password("{noop}123")
-                    .roles("USER");
+    protected void configure(AuthenticationManagerBuilder builder) throws Exception{
+        builder.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder ());
+        
     }
     
    @Override
